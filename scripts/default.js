d71c7dd13d3a343350eb2ed2d334ccf76ff9aeaa
LPhinney1 <logan.phinney10@gmail.com>
2023-12-20 16:15:16 -0500

    Update default.js

--- scripts/default.js
+++ scripts/default.js
@@ -1,19 +1,3 @@
-//get all sections and set initial display to home section
-/*const homeSection = document.getElementById('home');
-homeSection.style.display = 'block';
-const resumeSection = document.getElementById('restuarant-resume');
-resumeSection.style.display = 'none';
-
-//SECTION NAV LINK EVENT LISTENERS
-document.getElementById("homeLink").addEventListener("click", () => {
-    homeSection.style.display = 'block';
-    resumeSection.style.display = 'none';
-});
-document.getElementById("resumeLink").addEventListener("click", () => {
-    homeSection.style.display = 'none';
-    resumeSection.style.display = 'block';
-});*/
-
 $(document).ready(function () {
     const $homeSection = $('home');
     $homeSection.show();
