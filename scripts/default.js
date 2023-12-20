234665290b5ea1f12ef02a26025dd64ee64a998a
LPhinney1 <logan.phinney10@gmail.com>
2023-12-20 16:13:15 -0500

    5

--- scripts/default.js
+++ scripts/default.js
@@ -1,16 +1,34 @@
- //get all sections and set initial display
- const homeSection = document.getElementById('home');
- homeSection.style.display = 'block';
- const resumeSection = document.getElementById('restuarant-resume');
- resumeSection.style.display = 'none';
+//get all sections and set initial display to home section
+/*const homeSection = document.getElementById('home');
+homeSection.style.display = 'block';
+const resumeSection = document.getElementById('restuarant-resume');
+resumeSection.style.display = 'none';
 
- //SECTION NAV EVENT LISTENERS
- document.getElementById("homeLink").addEventListener("click", () => {
-         homeSection.style.display = 'block';
-         resumeSection.style.display = 'none';
- });
- document.getElementById("resumeLink").addEventListener("click", () => {
-     homeSection.style.display = 'none';
-     resumeSection.style.display = 'block';
- });
- 
+//SECTION NAV LINK EVENT LISTENERS
+document.getElementById("homeLink").addEventListener("click", () => {
+    homeSection.style.display = 'block';
+    resumeSection.style.display = 'none';
+});
+document.getElementById("resumeLink").addEventListener("click", () => {
+    homeSection.style.display = 'none';
+    resumeSection.style.display = 'block';
+});*/
+
+$(document).ready(function () {
+    const $homeSection = $('home');
+    $homeSection.show();
+
+    const $resumeSection = $('restaurant-resume');
+    $resumeSection.hide();
+
+    // Section Nav Link Event Listeners
+    $("homeLink").on("click", function () {
+        $homeSection.show();
+        $resumeSection.hide();
+    });
+
+    $("resumeLink").on("click", function () {
+        $homeSection.hide();
+        $resumeSection.show();
+    });
+});
