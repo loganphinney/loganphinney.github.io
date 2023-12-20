3912cfb4d592eac36e91fd45542d9cfd87138994
LPhinney1 <logan.phinney10@gmail.com>
2023-12-20 16:26:09 -0500

    7

--- scripts/default.js
+++ scripts/default.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
+    //SET INITIAL SECTION VISIBILITY
     const $homeSection = $("#home");
     $homeSection.show();
-
-    const $resumeSection = $("#restaurant-resume");
+    const $resumeSection = $("#resume");
     $resumeSection.hide();
 
-    // Section Nav Link Event Listeners
-    $("#homeLink").on("click", function () {
+    //NAV LINK ONCLICK FUNCTIONS
+    $("#homeLink").on("click", () => {
         $homeSection.show();
         $resumeSection.hide();
     });
 
-    $("#resumeLink").on("click", function () {
+    $("#resumeLink").on("click", () => {
         $homeSection.hide();
         $resumeSection.show();
     });
