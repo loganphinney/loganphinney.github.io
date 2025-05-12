7fb4877c0450c48eb8971fc5647b12bfb9c79d49
loganphinney <logan.phinney10@gmail.com>
2025-05-12 15:05:21 -0400

    added React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router';
 import './App.css';
 import MainPage from './components/MainPage';
 import ResumePage from './components/ResumePage';
 import GalleryPage from './components/GalleryPage';
 
 function App() {
+    const location = useLocation();
+    const navigate = useNavigate();
     const [currentPage, setCurrentPage] = useState<'main' | 'resume' | 'gallery'>('main');
+
+    useEffect(() => {
+        const path = location.pathname;
+        if (path === '/resume') {
+            setCurrentPage('resume');
+        } else if (path === '/gallery') {
+            setCurrentPage('gallery');
+        } else {
+            setCurrentPage('main');
+        }
+    }, [location]);
+
+    useEffect(() => {
+        if (currentPage === 'resume') {
+            navigate('/resume');
+        } else if (currentPage === 'gallery') {
+            navigate('/gallery');
+        } else {
+            navigate('/');
+        }
+    }, [currentPage, navigate]);
 
     return (
         <div className="App">
