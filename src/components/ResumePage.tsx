136c05e89c4291682151d508b90e81270bbbebe8
loganphinney <logan.phinney10@gmail.com>
2025-07-13 08:07:45 -0400

    edits

--- src/components/ResumePage.tsx
+++ src/components/ResumePage.tsx
@@ -14,8 +14,7 @@ const ResumePage: FC<ResumePageProps> = ({ navigate }) => {
                         Back to Home
                     </button>
                 </div>
-                <h3 className="camera-title">Résumé Not Available At This Time.</h3>
-                <h4>Sorry!</h4>
+                <h3 style={{ margin: "auto" }}>Not Available At This Time.<br></br>Sorry!</h3>
                 {/*<iframe src="/docs/LoganPhinney_Resume.pdf" title="Resume PDF" loading="lazy" width="100%" height="90%"></iframe>
                 <a href="/docs/LoganPhinney_Resume.pdf" download="LoganPhinney-Resume" target="_blank">
                     <button className="download-button">Download</button>
